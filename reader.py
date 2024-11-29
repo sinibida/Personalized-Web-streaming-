74062df517ee3e183ee59d0dcb30d185f2a6144f
SION001122 <163120715+SION001122@users.noreply.github.com>
2024-11-29 09:51:02 +0900

    Add files via upload
    오디오  디렉토리 추가해서 쓸것.

---
+++ reader.py
@@ -0,0 +1,25 @@
+import os
+base_path = os.path.dirname(os.path.abspath(__file__))
+audio_folder = os.path.join(base_path, 'audio')
+output_file = os.path.join(base_path, 'audio_file_list.txt')
+
+try:
+    if not os.path.exists(audio_folder):
+        print(f"폴더가 존재하지 않습니다: {audio_folder}")
+        exit()
+
+    files = os.listdir(audio_folder)
+    lines = []
+
+    # 파일 처리
+    for index, filename in enumerate(files, start=1):
+        file_path = os.path.join(audio_folder, filename)
+        if os.path.isfile(file_path):
+            formatted_line = f"{index}*file*{file_path}"
+            lines.append(formatted_line)
+    with open(output_file, 'w', encoding='utf-8') as f:
+        f.write('\n'.join(lines))
+    print(f"파일 경로가 성공적으로 저장되었습니다: {output_file}")
+
+except Exception as e:
+    print(f"오류 발생: {e}")
