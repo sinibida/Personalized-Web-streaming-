4c06c6402b351125842be54edbb620e27de47313
SION001122 <163120715+SION001122@users.noreply.github.com>
2024-11-29 18:14:17 +0900

    Update reader.py
    기여로 해주지 ㅜㅜ

--- reader.py
+++ reader.py
@@ -1,4 +1,3 @@
-#https://github.com/SION001122/Personalized-Web-streaming-/issues/3 여기서 공여받음.
 import os
 base_path = os.path.dirname(os.path.abspath(__file__))
 audio_folder = os.path.join(base_path, 'audio')
@@ -6,16 +5,15 @@ output_file = os.path.join(base_path, 'audio_file_list.txt')
 
 try:
     if not os.path.exists(audio_folder):
-        os.makedirs(audio_folder)
-        print(f"폴더가 존재하지 않아 생성되었습니다.: {audio_folder}")
+        print(f"폴더가 존재하지 않습니다: {audio_folder}")
+        exit()
 
     files = os.listdir(audio_folder)
     lines = []
 
-    # 파일 처리
     for index, filename in enumerate(files, start=1):
         file_path = os.path.join(audio_folder, filename)
-        if os.path.isfile(file_path):
+        if os.path.isfile(file_path) and os.path.splitext(file_path)[1].lower() in ['.aiff','.wav','.flac','.mp3']:
             formatted_line = f"{index}*file*{file_path}"
             lines.append(formatted_line)
     with open(output_file, 'w', encoding='utf-8') as f:
