3502b1b78a6d35dc4cfe5506bec348413dbfaefb
SION001122 <163120715+SION001122@users.noreply.github.com>
2024-12-03 12:26:04 +0900

    Delete 8000port.cmd

--- 8000port.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-REM Windows 방화벽에서 8000번 포트를 허용
-netsh advfirewall firewall add rule name="Allow Port 8000" dir=in action=allow protocol=TCP localport=8000
-echo Port 8000 is now open in the firewall.
-pause
